05cbdf25decd8f97d1af77fe0f8cb2d2a8d6b468
Aiko <karafish3r@gmail.com>
2021-12-10 20:39:34 +0100

    Initial Commit

---
+++ cowo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cowo
+//
+//  Created by Aiko Lebida on 10.12.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
